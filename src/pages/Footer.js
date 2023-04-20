bdec1fe041a21b0e0838e31c25787bbe83179942
gregorymark254@gmail.com <gregorymark254>
2023-04-21 00:02:13 +0300

    LANDING PAGE

--- src/pages/Footer.js
+++ src/pages/Footer.js
@@ -1,10 +1,39 @@
 import React from 'react'
+import { FaFacebookF,FaTwitter,FaPinterestP,FaInstagram } from "react-icons/fa";
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <main>
+      <div className="bg-[#f5f5f5]">
+        <div className="container mx-auto p-4">
+          <div className="flex flex-wrap justify-between">
+            <div className="p-4 space-y-3">
+              <h4>About Website</h4>
+              <p>He oppose at thrown desire of no. Announcing <br />impression unaffected day his are unreserved <br /> indulgence. Him hard find read are you</p>
+            </div>
+            <div className="p-4 space-y-3">
+              <h4>Links</h4>
+              <p>Important: Terms & Conditions, Privacy Policy</p>
+              <p>Useful: Colorpicker, Icon Library, Illustrations</p>
+              <p>Menu: Home, Details, Services, Contact</p>
+            </div>
+            <div className="p-4 text-left space-y-3 md:text-right">
+              <div className="flex space-x-5">
+                <h4><FaFacebookF/></h4>
+                <h4><FaTwitter/></h4>
+                <h4><FaPinterestP/></h4>
+                <h4><FaInstagram/></h4>
+              </div>
+              <p>We would love to hear from you</p>
+              <p>contact@site.com</p>
+            </div>
+          </div>
+          <div className="text-center p-4 mt-4">
+            <h6>Copyright &copy; By Gregory Mark.</h6>
+          </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
